fdf183800a24e81e5d98c7fe4c78395fbee43f9b
christinetrac <christineetrac@gmail.com>
2024-02-23 22:06:33 -0500

    specific product and best product screens

--- screens/search/BestProductScreen.js
+++ screens/search/BestProductScreen.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { BackButton, PrimaryButton } from "../../components/Buttons";
-import { RegularText } from "../../components/CustomText";
-import { Image, StyleSheet, View } from "react-native";
+import {BoldText, RegularText} from "../../components/CustomText";
+import {StyleSheet, TextInput, TouchableOpacity, View} from "react-native";
 import { TAB_OPTIONS } from "../../utils/constants";
 
 export const BestProductScreen = ({ navigation, route }) => {
     const query = route?.params?.query;
+    const [count, onCountChange] = useState("");
+    const [lowestPriceOption, setLowestPriceOption] = useState("price_item");
 
     const handleAddToList = () => {
         navigation.navigate(TAB_OPTIONS.list);
@@ -14,11 +16,40 @@ export const BestProductScreen = ({ navigation, route }) => {
     return (
         <View style={styles.container}>
             <BackButton onPress={() => navigation.pop()} />
-            <View style={styles.card}>
-                <Image source={require("../../assets/images/orange.png")} style={styles.image} />
+            <RegularText style={{ fontSize: 40, paddingLeft: 40, alignSelf: 'flex-start', paddingTop: 40 }}>Best Price</RegularText>
+            <RegularText style={{ fontSize: 20, paddingHorizontal: 40, alignSelf: 'flex-start', paddingTop: 10, paddingBottom: 20 }}>
+                The Best Price feature finds the lowest price for the selected product.
+                We will determine the <BoldText style={{color: "#445601"}}>{query}</BoldText> that best suit your needs.
+            </RegularText>
+            <RegularText style={{ fontSize: 24, paddingHorizontal: 40, alignSelf: 'flex-start', paddingTop: 10 }}>
+                Preferred Count
+            </RegularText>
+            <View style={styles.inputContainer}>
+                <View style={styles.inputLabel}>
+                    <RegularText style={{ fontSize: 20 }}>
+                        Item Count
+                    </RegularText>
+                </View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onCountChange}
+                    value={count}
+                    placeholder="Number"
+                    keyboardType="numeric"
+                    placeholderTextColor="#747474"
+                />
             </View>
-            <RegularText style={{ fontSize: 40, paddingLeft: 40, alignSelf: 'flex-start', paddingTop: 30 }}>{item.ProductName}</RegularText>
-            <RegularText style={{ fontSize: 20, paddingLeft: 40, alignSelf: 'flex-start', paddingTop: 18, paddingBottom: 20 }}>Price: {item.Price}</RegularText>
+            <RegularText style={{ fontSize: 24, paddingHorizontal: 40, alignSelf: 'flex-start', paddingTop: 10, paddingBottom: 10 }}>
+                Lowest Price Preference
+            </RegularText>
+            <View style={styles.quality}>
+                <TouchableOpacity style={lowestPriceOption === 'price_item' ? styles.qualityButtonActive : styles.qualityButton} onPress={() => setLowestPriceOption('price_item')}>
+                    <BoldText style={lowestPriceOption === 'price_item' ? styles.qualityTextActive : styles.qualityText}>Item Price</BoldText>
+                </TouchableOpacity>
+                <TouchableOpacity style={lowestPriceOption === 'price_per_unit' ? styles.qualityButtonActive : styles.qualityButton} onPress={() => setLowestPriceOption('price_per_unit')}>
+                    <BoldText style={lowestPriceOption === 'price_per_unit' ? styles.qualityTextActive : styles.qualityText}>Price Per Unit</BoldText>
+                </TouchableOpacity>
+            </View>
             <PrimaryButton label="add to list" onPress={handleAddToList} />
         </View>
     )
@@ -28,29 +59,88 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F6FFF1',
-        paddingTop: 90,
+        paddingTop: 80,
         paddingBottom: 20,
         position: 'relative',
         alignItems: 'center',
     },
-    card: {
-        width: 310,
-        height: 220,
-        backgroundColor: '#fff',
-        borderRadius: 14,
-        shadowColor: '#DCDCDC',
-        shadowOffset: {
-            width: 10,
-            height: 9
-        },
-        shadowOpacity: 1,
-        shadowRadius: 0,
+    inputContainer: {
         display: "flex",
+        flexDirection: "row",
         justifyContent: "center",
-        alignItems: "center"
+        gap: -24,
+        width: "100%",
+        marginBottom: 20,
+        marginTop: -5
     },
-    image: {
-        height: 200,
-        width: 220,
+    inputLabel: {
+        height: 56,
+        margin: 12,
+        borderWidth: 2,
+        borderColor: "#9D9D9D",
+        backgroundColor: "#FFF",
+        borderTopLeftRadius: 12,
+        borderBottomLeftRadius: 12,
+        borderRightWidth: 0,
+        justifyContent: "center",
+        paddingLeft: 18,
+        width: 140
+    },
+    input: {
+        height: 56,
+        margin: 12,
+        borderWidth: 2,
+        borderColor: "#9D9D9D",
+        padding: 18,
+        backgroundColor: "#FFF",
+        borderTopRightRadius: 12,
+        borderBottomRightRadius: 12,
+        color: "#747474",
+        fontSize: 20,
+        fontFamily: 'Inter_400Regular',
+        borderLeftWidth: 0,
+        width: 165,
+        zIndex: 99
+    },
+    quality: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-evenly",
+        width: "100%",
+        marginBottom: 80
+    },
+    qualityButton: {
+        height: 80,
+        width: 140,
+        borderRadius: 12,
+        backgroundColor: "#fff",
+        borderWidth: 2,
+        borderColor: "#9D9D9D",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        padding: 12
+    },
+    qualityButtonActive: {
+        height: 80,
+        width: 140,
+        backgroundColor: "#fff",
+        borderRadius: 12,
+        borderWidth: 2,
+        borderColor: "#445601",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        padding: 12
+    },
+    qualityText :{
+        fontSize: 20,
+        color: "#747474",
+        textAlign: "center",
+    },
+    qualityTextActive :{
+        fontSize: 20,
+        textAlign: "center",
+        color: "#445601"
     },
 });
--- screens/search/ProductScreen.js
+++ screens/search/ProductScreen.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import { BackButton, PrimaryButton } from "../../components/Buttons";
-import { RegularText } from "../../components/CustomText";
-import { Image, StyleSheet, View } from "react-native";
+import {BoldText, RegularText} from "../../components/CustomText";
+import {Image, ScrollView, StyleSheet, TextInput, View} from "react-native";
 import { TAB_OPTIONS } from "../../utils/constants";
 
 export const ProductScreen = ({ navigation, route }) => {
     const item = route?.params?.item;
+    const [count, onCountChange] = useState("");
 
     const handleAddToList = () => {
         navigation.navigate(TAB_OPTIONS.list);
     }
 
     return (
-        <View style={styles.container}>
-            <BackButton onPress={() => navigation.pop()} />
-            <View style={styles.card}>
-                <Image source={require("../../assets/images/orange.png")} style={styles.image} />
+        <ScrollView style={{flex: 1, backgroundColor: '#F6FFF1'}}>
+            <View style={styles.container}>
+                <BackButton onPress={() => navigation.pop()} />
+                <View style={styles.card}>
+                    <View style={styles.badge}>
+                        <BoldText style={{fontSize:12, textTransform:"uppercase", color: "#445601"}}>
+                            {item.Store}
+                        </BoldText>
+                    </View>
+                    <Image source={item.Picture} style={styles.image} />
+                </View>
+                <RegularText style={{ fontSize: 32, alignSelf: 'flex-start', paddingTop: 20, paddingBottom: 20 }}>{item.ProductName}</RegularText>
+                <RegularText style={{ fontSize: 24, alignSelf: 'flex-start', paddingTop: 10 }}>
+                    Preferred Count
+                </RegularText>
+                <View style={styles.inputContainer}>
+                    <View style={styles.inputLabel}>
+                        <RegularText style={{ fontSize: 20 }}>
+                            Item Count
+                        </RegularText>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onCountChange}
+                        value={count}
+                        placeholder="Number"
+                        keyboardType="numeric"
+                        placeholderTextColor="#747474"
+                    />
+                </View>
+                <RegularText style={{ fontSize: 24, alignSelf: 'flex-start' }}>Price: $9.99</RegularText>
+                <RegularText style={{ fontSize: 24, alignSelf: 'flex-start', paddingBottom: 20 }}>Price Per Unit: $0.55</RegularText>
+                <PrimaryButton label="add to list" onPress={handleAddToList} />
             </View>
-            <RegularText style={{ fontSize: 40, paddingLeft: 40, alignSelf: 'flex-start', paddingTop: 30 }}>{item.ProductName}</RegularText>
-            <RegularText style={{ fontSize: 20, paddingLeft: 40, alignSelf: 'flex-start', paddingTop: 18, paddingBottom: 20 }}>Price: {item.Price}</RegularText>
-            <PrimaryButton label="add to list" onPress={handleAddToList} />
-        </View>
+        </ScrollView>
     )
 }
 
@@ -30,18 +57,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#F6FFF1',
         paddingTop: 90,
         paddingBottom: 20,
+        paddingHorizontal: 40,
         position: 'relative',
         alignItems: 'center',
     },
     card: {
+        marginVertical: 20,
         width: 310,
-        height: 220,
+        height: 210,
         backgroundColor: '#fff',
         borderRadius: 14,
         shadowColor: '#DCDCDC',
         shadowOffset: {
-            width: 10,
-            height: 9
+            width: 12,
+            height: 12
         },
         shadowOpacity: 1,
         shadowRadius: 0,
@@ -50,7 +79,65 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     image: {
-        height: 200,
-        width: 220,
+        flex: 1,
+        width: "100%",
+        height: undefined,
+        resizeMode: 'contain',
+        justifyContent: "center",
+        alignSelf: "center",
     },
+    inputContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: -24,
+        width: "100%",
+        marginBottom: 20,
+        marginTop: -5
+    },
+    inputLabel: {
+        height: 56,
+        margin: 12,
+        borderWidth: 2,
+        borderColor: "#9D9D9D",
+        backgroundColor: "#FFF",
+        borderTopLeftRadius: 12,
+        borderBottomLeftRadius: 12,
+        borderRightWidth: 0,
+        justifyContent: "center",
+        paddingLeft: 18,
+        width: 140
+    },
+    input: {
+        height: 56,
+        margin: 12,
+        borderWidth: 2,
+        borderColor: "#9D9D9D",
+        padding: 18,
+        backgroundColor: "#FFF",
+        borderTopRightRadius: 12,
+        borderBottomRightRadius: 12,
+        color: "#747474",
+        fontSize: 20,
+        fontFamily: 'Inter_400Regular',
+        borderLeftWidth: 0,
+        width: 165,
+        zIndex: 99
+    },
+    badge: {
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+        backgroundColor: "#fff",
+        borderRadius: 14,
+        borderWidth: 2,
+        borderColor: "#445601",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "flex-start",
+        zIndex: 99,
+        position: "absolute",
+        top: 0,
+        left: 0
+    }
 });
